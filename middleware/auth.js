04321993fce34c2d12d1f1f27a508b4c49d268db
Andrew Guo <andrewguo2001@gmail.om>
2021-02-01 15:23:05 -0500

    Added authentication with jsonwebtokens

---
+++ middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken'); 
+const config = require('config'); 
+
+module.exports = function(req, res, next) {
+  // Get Token from Header 
+  const token = req.header('x-auth-token');
+
+  // Check if token exists 
+  if(!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied.' }); 
+  }
+  // Verify Token 
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret')); 
+
+    req.user = decoded.user; // Set user to be the decoded token 
+    next(); 
+  } catch (error) {
+    res.status(401).json({ msg: 'Token is not valid, authorization denied.' }); 
+  }
+}
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,19 @@
 const express = require('express'); 
 const router = express.Router(); 
+const auth = require('../../middleware/auth'); // Authenticates Token 
+const User = require('../../models/User'); 
 
 // @route  GET api/auth
 // @desc   Testing Route 
-// @access Public 
-router.get('/', (req, res) => res.send('auth Route')); 
+// @access Protected  
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password'); // Returns all except password  
+    res.json(user); 
+  } catch (error) {
+    console.error(error.message); 
+    res.status(500).send('Server Error'); 
+  }
+}); 
 
 module.exports = router; 
