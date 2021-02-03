73306843ba9f6b8d4fe405a51f0ece8366295bff
Andrew Guo <andrewguo2001@gmail.om>
2021-02-03 10:15:40 -0500

    Changed baseURL of axios so that it can make API requests to backend

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios'; 
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,6 +10,9 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// Set to 3000 so axios will not go to App.js base URL to make API Requests 
+axios.defaults.baseURL = 'http://localhost:3000'; 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
