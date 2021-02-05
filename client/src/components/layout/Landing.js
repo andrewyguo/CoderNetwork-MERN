7eea7d2b8febc738923a5c6fcb252c3d73a48280
Andrew Guo <andrewguo2001@gmail.com>
2021-02-05 10:27:53 -0500

    Added redirect from landing page if user is logged in

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,7 +1,13 @@
-import React from 'react'
-import { Link } from 'react-router-dom'; 
+import React from 'react'; 
+import { Link, Redirect } from 'react-router-dom'; 
+import { connect } from 'react-redux'; 
+import PropTypes from 'prop-types'; 
 
-export const Landing = () => {
+export const Landing = ({ isAuthenticated }) => {
+  if(isAuthenticated) {
+    return <Redirect to='/dashboard'></Redirect>
+  }
+
   return (
     <section className="landing">
       <div className="dark-overlay">
@@ -19,6 +25,13 @@ export const Landing = () => {
       </div>
     </section>
   )
-}
+}; 
 
-export default Landing;
+Landing.propTypes = { 
+  isAuthenticated: PropTypes.bool 
+}
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+}); 
+
+export default connect(mapStateToProps)(Landing);
