4a0fa078c3b7b51115369ece18b60012ff140689
Andrew Guo <andrewguo2001@gmail.com>
2021-03-06 18:29:25 -0500

    changed server.js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express'); 
 const connectDB = require('./config/db'); 
+const path = require('path');
 
 const app = express(); 
 
 // Connect DB 
 connectDB(); 
 
-app.get('/', (req, res) => res.send('API Running')); 
+// app.get('/', (req, res) => res.send('API Running')); 
 
 // Init Middleware 
 app.use(express.json({ extended: false })); 
@@ -24,6 +25,16 @@ app.use('/api/posts', require('./routes/api/posts'));
 //   res.append('Access-Control-Allow-Headers', ['Content-Type', 'x-auth-token']);
 //   next();
 // }); 
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000; 
 
