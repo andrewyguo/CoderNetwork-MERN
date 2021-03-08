21191ddfe8318be52812ddf5564a934a28995bd6
Andrew Guo <andrewguo2001@gmail.com>
2021-03-08 09:43:31 -0500

    Changed procfile back to original and updated uuid to uuidv4()

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -2,7 +2,7 @@ import { v4 as uuid } from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from './types'; 
 
 export const setAlert = (msg, alertType, timeout = 3600) => dispatch => {
-  const id = uuid; 
+  const id = uuidv4();
   dispatch({
     type: SET_ALERT,
     payload: { msg, alertType, id }
@@ -11,4 +11,4 @@ export const setAlert = (msg, alertType, timeout = 3600) => dispatch => {
 
   // Remove alert 
   setTimeout(() => dispatch( { type: REMOVE_ALERT, payload: id }), timeout)
-}   
+}  
